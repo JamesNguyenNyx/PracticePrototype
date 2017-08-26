54c879d4e79e43e8423951c8515cd37294357543
James Nguyen <jamesnguyen.nyx@gmail.com>
2017-08-26 21:17:13 +0700

    Add Base components

---
+++ Camelia/Camelia/Classes/Base/Components/BaseTextField.swift
@@ -0,0 +1,14 @@
+//
+//  BaseTextField.swift
+//  Camelia
+//
+//  Created by James Nguyen on 2017/08/26.
+//  Copyright © 2017 James Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class BaseTextField: UITextField, Jitterable{
+
+
+}
