a90dd5c0f757d10b5d401e8f4b0e75fdb5efc923
James Nguyen <jamesnguyen.nyx@gmail.com>
2017-08-28 23:38:41 +0700

    Add model

---
+++ Camelia/Camelia/Classes/Model/PageModel.swift
@@ -0,0 +1,17 @@
+//
+//  PageModel.swift
+//  Camelia
+//
+//  Created by James Nguyen on 2017/08/28.
+//  Copyright © 2017 James Nguyen. All rights reserved.
+//
+
+import Foundation
+
+struct PageModel {
+    
+    let title: String
+    let message: String
+    let imageName: String
+    
+}
